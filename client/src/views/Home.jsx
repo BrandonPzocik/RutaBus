eeb3d69171119a7e7951a5139b6289c11f34443a
BrandonPzocik <pzocikbrandon@gmail.com>
2026-01-17 01:35:41 -0300

    se elimino la funcionalidad del foro

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { MapPage } from "./Maps";
-import Forum from "../components/Forum";
 
 
 export default function Home() {
@@ -11,8 +10,6 @@ export default function Home() {
             <Header />
             <div className="h-[80vh] "> {/* Asegura que el mapa tenga un espacio adecuado */}
                 <MapPage />
-
-                <Forum />
             </div>
             
         </>
