dc2a99257b066763e3c98429046406166b8a6d41
BrandonPzocik <pzocikbrandon@gmail.com>
2026-01-17 23:25:14 -0300

    arreglando el ruteo con el token

--- client/src/routes/AppRouter.jsx
+++ client/src/routes/AppRouter.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useContext } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+
 import Landing from "../views/Landing";
 import { ReclamosPeticiones } from "../views/ReclamosPeticiones.jsx";
 import { Login } from "../views/Login";
@@ -10,8 +12,7 @@ import Profile from "../views/Profile.jsx";
 import MapaPage from "../views/mapa.jsx";
 
 const AppRouter = () => {
-  // Almacenar el token
-  const token = localStorage.getItem("token");
+  const { token } = useContext(AuthContext);
 
   return (
     <BrowserRouter>
@@ -20,21 +21,23 @@ const AppRouter = () => {
           path="/"
           element={token ? <Navigate to="/home" /> : <Landing />}
         />
-        {/* Si hay token, redirigir a home, de lo contrario, mostrar Login */}
+
         <Route
           path="/login"
           element={token ? <Navigate to="/home" /> : <Login />}
         />
-        <Route path="/reclamos" element={<ReclamosPeticiones />} />
-        {/* Si hay token, mostrar Home, de lo contrario, redirigir a Login */}
+
         <Route
           path="/home"
           element={token ? <Home /> : <Navigate to="/login" />}
         />
+
         <Route
           path="/profile"
           element={token ? <Profile /> : <Navigate to="/" />}
         />
+
+        <Route path="/reclamos" element={<ReclamosPeticiones />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/mapa" element={<MapaPage />} />
